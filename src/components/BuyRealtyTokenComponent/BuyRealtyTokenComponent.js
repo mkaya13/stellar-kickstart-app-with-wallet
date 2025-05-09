8ef19d5b6f2a53d9c10288ff552ab07817bf9b2c
mkaya13 <mertkaya783@hotmail.com>
2025-05-09 11:38:46 +0300

    Finalized BuyRealtyTokenComponent

--- src/components/BuyRealtyTokenComponent/BuyRealtyTokenComponent.js
+++ src/components/BuyRealtyTokenComponent/BuyRealtyTokenComponent.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import {
     TransactionBuilder,
     Account,
@@ -14,11 +15,16 @@ const CONTRACT_PUBLIC_KEY = process.env.REACT_APP_CONTRACT_PUBLIC_KEY;
 const ASSET_CONTRACT_ADDRESS = process.env.REACT_APP_ASSET_CONTRACT_ADDRESS;
 
 export default function BuyRealtyTokenComponent({ publicKey, kit }) {
+  const [buyAmount, setBuyAmount] = useState('');
+  const [submittedBuyAmount, setSubmittedBuyAmount] = useState('');
+
     const BuyRealtyTokenHandler = async () => {
+
       if (!publicKey) {
           alert("🚫 Wallet not connected.");
           return;
       }
+
       // 1. Define Credentials
       const accountResponse = await axios.get(`${RPC_URL}/accounts/${publicKey}`);
       const sequence = accountResponse.data.sequence;
@@ -26,16 +32,12 @@ export default function BuyRealtyTokenComponent({ publicKey, kit }) {
       const contract = new Contract(CONTRACT_PUBLIC_KEY);
       const account = new Account(publicKey, sequence);
 
-      console.log("1")
-
       // 2. Convert Function Parameter to nativeToScVal
 
       const raw_asset_address = nativeToScVal(ASSET_CONTRACT_ADDRESS, { type: 'address' });
       const raw_buyer_address = nativeToScVal(publicKey, { type: 'address' });
-      const raw_asset_amount = nativeToScVal("20000000", { type: 'i128' });
+      const raw_asset_amount = nativeToScVal((Number(buyAmount)* 10000000).toString(), { type: 'i128' });
       
-      console.log("2")
-
       // 3. Built the Transaction
 
       let tx = new TransactionBuilder(account, {
@@ -45,8 +47,6 @@ export default function BuyRealtyTokenComponent({ publicKey, kit }) {
         .addOperation(contract.call('buy_realty_token', raw_asset_address, raw_buyer_address, raw_asset_amount))
         .setTimeout(30)
         .build();
-
-      console.log("3")
 
       // 4. Simulate the Response
 
@@ -63,26 +63,18 @@ export default function BuyRealtyTokenComponent({ publicKey, kit }) {
           }
       );
 
-      console.log("4 simResponse", simResponse)
-
       // 5. Get the transactionData
 
       const transactionData = simResponse.data.result.transactionData;
-
-      console.log("5")
 
       // 6. Decode a base64-encoded string (transactionData) into a SorobanTransactionData object
 
       const sorobanData = xdr.SorobanTransactionData.fromXDR(transactionData, 'base64');
 
-      console.log("6")
-
       // 7. Grap the Updated Fee
 
       const updatedFee = (parseInt(simResponse.data.result.minResourceFee) + 100).toString();
       const txnAccount = new Account(publicKey, sequence);
-
-      console.log("7")
 
       // 8. Build the Transaction Data with the Simulated Transaction Response
 
@@ -98,32 +90,26 @@ export default function BuyRealtyTokenComponent({ publicKey, kit }) {
         ))
         .setTimeout(30)
         .setSorobanData(sorobanData)
-        .build();      
-      
+        .build();    
+        
+      // 9. Add authEntries into the transaction (for require.auth(buyer))
       
       const authEntries = simResponse.data.result.results[0].auth.map((entryXdr) =>
         xdr.SorobanAuthorizationEntry.fromXDR(entryXdr, "base64")
       );
 
-      // tx.operations[0].auth = authEntries;
-
-      // 2. Add into operation call (the raw XDR operation)
+      // Add into operation call (the raw XDR operation)
       tx._tx._attributes.operations[0]._attributes.body._value._attributes.auth = authEntries;
 
-      console.log("Latest tx:", tx)
-
-      // tx.tx._attributes.sorobanAuth = authEntries; // ✅ ✅ ✅ now valid
-
-      // console.log("Final TX", tx);
             
-      // 9. Ask StellarWalletsKit to sign the transaction
+      // 10. Ask StellarWalletsKit to sign the transaction
       
       const { signedTxXdr } = await kit.signTransaction(tx.toXDR(), {
         address: publicKey,
         networkPassphrase: NETWORK_PASSPHRASE,
       });
 
-      // 10. Submit the Transaction Request
+      // 11. Submit the Transaction Request
 
       const submitResponse = await axios.post(
           RPC_URL,
@@ -138,14 +124,28 @@ export default function BuyRealtyTokenComponent({ publicKey, kit }) {
           }
       );
 
-      console.log("submitResponse:", submitResponse)
+      // ✅ Update submittedUserName and clear input
+      setSubmittedBuyAmount(buyAmount);
+      setBuyAmount('');
       
   }
 
   return (
     <div className="buy-realty-token-feature">
       <h1>Buy Realty Token Component</h1>
-        <button onClick={BuyRealtyTokenHandler}>Click to buy</button>
+      <input
+        type="number"
+        placeholder="Enter Buy Amount"
+        value={buyAmount}
+        onChange={(e) => setBuyAmount(e.target.value)}
+      />
+      <button onClick={BuyRealtyTokenHandler}>Buy</button>
+
+      {setSubmittedBuyAmount && (
+        <p style={{ color: 'green' }}>
+          ✅ {submittedBuyAmount} Tokens Buy Amount Submitted <strong>{setSubmittedBuyAmount}</strong>
+        </p>
+      )}
     </div>
   );
 }
